696aa5da1c4d148c3277be901e0bbdf635932e2a
Gilbert Ngeno <ngenogilbert07@gmail.com>
2022-01-28 13:04:00 +0300

    React Add Employee Form handling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import AddEmployeeComponent from "./components/AddEmployeeComponent";
 import FooterComponent from "./components/FooterComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
@@ -13,6 +14,10 @@ function App() {
           <Routes>
             <Route exact path="/" component={ListEmployeeComponent}></Route>
             <Route path="/employees" component={ListEmployeeComponent}></Route>
+            <Route
+              path="/add-employee"
+              component={AddEmployeeComponent}
+            ></Route>
           </Routes>
           <ListEmployeeComponent />
         </div>
--- src/components/ListEmployeeComponent.js
+++ src/components/ListEmployeeComponent.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import EmployeeService from "../services/EmployeeService";
 
 const ListEmployeeComponent = () => {
@@ -18,6 +19,11 @@ const ListEmployeeComponent = () => {
   return (
     <div className="container">
       <h2 className="text-center"> List Employees</h2>
+
+      <Link to="/add-employee" className="btn btn-primary mb-2">
+        {" "}
+        Add Employee
+      </Link>
 
       <table className="table table-bordered table-striped">
         <thead>
